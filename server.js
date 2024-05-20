db32a6d5cf23499f14ba7ae1076d5388b21f11b5
Klaus <klazapp@klazapp.com>
2024-05-20 15:00:59 +0800

    Renamed src to api

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Import the configured Express application.
-const app = require("./src/app");
+const app = require("./api/app");
 //Node.js HTTP module to create an HTTP server.
 const http = require("http");
 // Set the port from environment variables or default to 3000.
